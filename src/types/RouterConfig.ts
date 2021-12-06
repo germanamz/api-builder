7bff09a1c4840ddf4e5f25748099b4f3febd3453
German Meza <german@feprisa.com>
2021-12-06 11:31:09 -0600

    feat: router action configs optional methods

--- src/types/RouterConfig.ts
+++ src/types/RouterConfig.ts
@@ -10,7 +10,7 @@ type RouterConfig = {
     [key: string]: string;
   };
   actionConfig: {
-    [method in SupportedHttpMethods]: ActionConfig;
+    [method in SupportedHttpMethods]?: ActionConfig;
   };
   policyStatements: Statement[];
 };
