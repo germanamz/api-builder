8a9be93e1b95e43253a7813262c1487a451e1a17
German Meza <iam@germanamz.com>
2022-06-03 13:13:28 -0500

    feat: added multi value entries to entries to object

--- packages/utils/src/helpers/entriesToObject.ts
+++ packages/utils/src/helpers/entriesToObject.ts
@@ -2,7 +2,15 @@ const entriesToObject = (entries: Iterable<[any, any]>) => {
   const obj: Record<any, any> = {};
 
   for (const [key, val] of entries) {
-    obj[key] = val;
+    if (obj[key]) {
+      if (Array.isArray(obj[key])) {
+        obj[key].push(val);
+      } else {
+        obj[key] = [obj[key], val];
+      }
+    } else {
+      obj[key] = val;
+    }
   }
 
   return obj;
