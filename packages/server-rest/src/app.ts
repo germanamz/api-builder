68ef2f49ff5ca374db0205a25d31699bb11b8ce4
German Meza <iam@germanamz.com>
2022-06-03 12:24:22 -0500

    feat: body parser removed for a custom middleware

--- packages/server-rest/src/app.ts
+++ packages/server-rest/src/app.ts
@@ -1,12 +1,19 @@
-import bodyParser from 'body-parser';
 import express from 'express';
 
 const app = express();
 
-app.use(
-  bodyParser.raw({
-    type: '*/*',
-  })
-);
+app.use((req, res, next) => {
+  const bodyData: Buffer[] = [];
+
+  req.on('data', (chunk) => {
+    bodyData.push(chunk);
+  });
+  req.on('end', () => {
+    if (bodyData.length) {
+      req.body = Buffer.concat(bodyData);
+    }
+    next();
+  });
+});
 
 export default app;
